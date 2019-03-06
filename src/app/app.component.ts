0a6c837913d810e4c4e9fc74616893127ea37f3c
JCarlosSisDev <jcarlos.quisbert.sis@gmail.com>
2019-03-06 01:39:13 -0400

    Construccion del metodo eliminar y configuracion de alertas. Los registros ya se editan y eliminan.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string = 'Proyecto Angular';
+  msg:string = '';
 
   empleados = [
     {'name': 'Juan', position: 'Encargado', email: 'email_1@test.com'},
@@ -16,17 +17,24 @@ export class AppComponent {
 
   model:any = {};
   model_up:any = {};
+  hideUpdate:boolean = true;
 
   addEmpleado():void{
     this.empleados.push(this.model);
+    this.msg = "El registro se realizó satisfactoriamente.";
   }
 
-  deleteEmpleado():void{
-
+  deleteEmpleado(i):void{
+    var answer = confirm('Desea eliminar el registro?');
+    if(answer){
+      this.empleados.splice(i, 1);
+      this.msg = "El registro se eliminó satisfactoriamente.";
+    }
   }
 
   myValue;
   editEmpleado(i):void{
+    this.hideUpdate = false;
     this.model_up.name = this.empleados[i].name;
     this.model_up.position = this.empleados[i].position;
     this.model_up.email = this.empleados[i].email;
@@ -39,8 +47,14 @@ export class AppComponent {
     for(let j = 0; j < this.empleados.length; j++){
       if(i == j){
         this.empleados[i] = this.model_up;
+        this.msg = "Los cambios se realizaron de forma satisfactoria.";
+        this.hideUpdate = true;
         this.model_up = {};
       }
     }
+  }
+
+  closeAlert():void{
+    this.msg = "";
   }
 }
